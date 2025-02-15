9d2427ce12823b6c04021f0a4a5584825d65c089
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-02-16 02:34:50 +0530

    Added index.jsx to centralize and manage all icon components for easy usage

---
+++ Front End/assets/icons/index.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Home from './Home'
+import { theme } from '../../constants/theme'
+import ArrowLeft from './ArrowLeft'
+import Mail from './Mail'
+import Lock from './Lock'
+import User from './User'
+import ViewTrue from './Viewture'
+import ViewFalse from './Viewfalse'
+import Calendar from './Calendar'
+import Contact from './Contact'
+
+const icons = {
+    home: Home,
+    arrowLeft: ArrowLeft,
+    mail: Mail,
+    lock: Lock,
+    user: User,
+    viewTrue: ViewTrue,
+    viewFalse: ViewFalse,
+    calendar: Calendar,
+    contact: Contact,
+}
+
+const Icon = ({name, ...props}) => {
+    const IconComponent = icons[name];
+  return (
+    <IconComponent
+        height={props.size || 24}
+        width={props.size || 24}
+        strokeWidth={props.strokeWidth || 1.9}
+        color={theme.colors.textWhite}
+        {...props}
+    />
+  )
+}
+
+export default Icon
+
+const styles = StyleSheet.create({})
