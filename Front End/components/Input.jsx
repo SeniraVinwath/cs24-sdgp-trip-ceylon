d190a9ab4e2aed79fe32e20f7b01ac5e1a80309e
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-02-19 20:15:20 +0530

    Updated Input.jsx with Pressable component for better interaction and picker support

--- Front End/components/Input.jsx
+++ Front End/components/Input.jsx
@@ -1,29 +1,40 @@
-import { StyleSheet, TextInput, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { theme } from '../constants/theme'
-import { hp } from '../helpers/common'
+import { StyleSheet, TextInput, View, TouchableOpacity, Text, Pressable } from 'react-native';
+import React from 'react';
+import { theme } from '../constants/theme';
+import { hp } from '../helpers/common';
 
 const Input = (props) => {
   return (
     <View style={[styles.container, props.containerStyles]}>
       {props.icon && React.cloneElement(props.icon, { color: '#475569' })}
+      
+      {props.picker && (
+        <Pressable style={styles.pickerContainer}>
+          <Text style={styles.selectedCode}>{props.selectedValue}</Text>
+          {props.picker}
+        </Pressable>
+      )}
+      
       <TextInput
-        style={styles.textInput}
+        style={[
+          styles.textInput,
+          props.picker && styles.textInputWithPicker
+        ]}
         placeholderTextColor="#94A3B8"
         secureTextEntry={props.secureTextEntry}
-        ref={props.inputRef}
         {...props}
       />
+      
       {props.rightIcon && (
         <TouchableOpacity onPress={props.onRightIconPress}>
           {props.rightIcon}
         </TouchableOpacity>
       )}
     </View>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
 
 const styles = StyleSheet.create({
   container: {
@@ -37,8 +48,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 18,
     gap: 12
   },
+  pickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    minWidth: 60,
+    height: '100%',
+    justifyContent: 'center',
+  },
+  selectedCode: {
+    color: '#000',
+    fontSize: 16,
+  },
   textInput: {
     flex: 1,
     color: '#000'
+  },
+  textInputWithPicker: {
+    marginLeft: 0
   }
-})
+});
