8d2885d39564b63d848ae6ac59c2c183a39d74cd
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-03-19 21:42:32 +0530

    feat: Add API to download post media in imageService.js

--- Front End/services/imageService.js
+++ Front End/services/imageService.js
@@ -18,6 +18,51 @@ export const getSupabaseFileUrl = filePath => {
     return null;
 }
 
+export const downloadFile = async (url) => {
+    try {
+        console.log("Downloading from URL:", url);
+        
+        const fileUri = getLocalFilePath(url);
+        console.log("Saving to:", fileUri);
+        
+        const fileInfo = await FileSystem.getInfoAsync(fileUri);
+        if (fileInfo.exists) {
+            return fileUri;
+        }
+        
+        const dirPath = FileSystem.documentDirectory;
+        const dirInfo = await FileSystem.getInfoAsync(dirPath);
+        if (!dirInfo.exists) {
+            await FileSystem.makeDirectoryAsync(dirPath, { intermediates: true });
+        }
+        
+        const result = await FileSystem.downloadAsync(url, fileUri);
+        
+        const downloadedFileInfo = await FileSystem.getInfoAsync(fileUri);
+        if (downloadedFileInfo.exists && downloadedFileInfo.size > 0) {
+            return fileUri;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error("Download failed:", error);
+        return null;
+    }
+}
+
+export const getLocalFilePath = filePath => {
+
+    let fileName = filePath.split('/').pop();
+    if (!fileName.includes('.')) {
+        if (filePath.includes('postImages')) {
+            fileName += '.jpg';
+        } else if (filePath.includes('postVideos')) {
+            fileName += '.mp4';  
+        }
+    }
+    return `${FileSystem.documentDirectory}${fileName}`;
+}
+
 export const uploadFile =  async (folderName, fileUri, isImage=true)=>{
     try{
         let fileName = getFilePath(folderName, isImage);
