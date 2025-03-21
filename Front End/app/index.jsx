ec9675c76592dc32d9b61f464d725ff885a929e8
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-03-21 18:19:22 +0530

    chore: Update loading screen styling with theme background

--- Front End/app/index.jsx
+++ Front End/app/index.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { useRouter } from 'expo-router'
 import ScreenWrapper from '../components/ScreenWrapper'
 import Loading from '../components/Loading'
+import { theme } from '../constants/theme'
 
 const index = () => {
     const router = useRouter();
   return (
-    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center', backgroundColor: theme.colors.themebg}}>
       <Loading />
     </View>
   )
