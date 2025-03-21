26fec76f1450c488dd67c9a940261c4c8690709a
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-03-21 18:23:45 +0530

    feat: Add notificationService.js to handle APIs for fetching notifications

---
+++ Front End/services/notificationService.js
@@ -0,0 +1,46 @@
+import { supabase } from "../lib/supabase"
+
+export const createNotification = async (notification) => {
+    try{
+        const {data, error} = await supabase
+        .from('notifications')
+        .insert(notification)
+        .select()
+        .single();
+
+        if(error){
+            console.log('notification error: ', error);
+            return {success: false, msg: 'Something went wrong!'};
+        }
+
+        return {success: true, data: data}
+    }catch(error){
+        console.log('notification error: ', error);
+        return {success: false, msg: 'Something went wrong!'};
+    }
+}
+
+export const fetchNotifications = async (recieverId) => {
+    try {
+        const {data,error} = await supabase
+        .from('notifications')
+        .select(`
+            *,
+            sender: senderId(id, user_name, image)
+            `)
+            .eq('recieverId', recieverId)
+            .order('created_at', {ascending: false});
+
+            if(error){
+                console.log('fetchNotifications error: ', error);
+                return {success: false, msg: 'Could not fetch notifications'};
+            }
+
+            return {success: true, data: data};
+
+    }catch(error){
+        console.log('fetchNotifications error: ', error);
+        return {success: false, msg: 'Could not fetch notifications'}
+
+    }
+}
