b794a2af4f35be5cb05b2043c2087db7126066f6
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-03-29 20:16:58 +0530

    chore: Add Dockerfile for backend deployment setup

--- Front End/constants/baseApiUrl.js
+++ Front End/constants/baseApiUrl.js
@@ -1,1 +1,2 @@
-export default BASE_API_IRL = "http://10.158.21.14:3000";
+//export default BASE_API_IRL = "http://10.158.21.14:3000";
+export default BASE_API_IRL = "cs24-sdgp-trip-ceylon-production.up.railway.app";
