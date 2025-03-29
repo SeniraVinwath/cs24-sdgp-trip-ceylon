6303571618893243ac2ae3e928a8adf7505dec0c
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-03-29 20:47:55 +0530

    chore: Restore accidentally removed directory folders and commit all staged changes

--- Front End/constants/baseApiUrl.js
+++ Front End/constants/baseApiUrl.js
@@ -1,2 +1,3 @@
 //export default BASE_API_IRL = "http://10.158.21.14:3000";
+//export default BASE_API_IRL = "cs24-sdgp-trip-ceylon-production-b031.up.railway.app";
 export default BASE_API_IRL = "cs24-sdgp-trip-ceylon-production-b031.up.railway.app";
