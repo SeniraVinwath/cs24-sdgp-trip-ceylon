ddfa9a5006fee4263454d3f4b2efca3b089dca1a
Juliyan Senira Vinwath Mendis <senira.20220997@iit.ac.lk>
2025-03-31 04:51:38 +0530

    chore: Remove accidentally committed secrets for security

--- Front End/constants/baseApiUrl.js
+++
@@ -1,2 +0,0 @@
-//export default BASE_API_IRL = "http://10.158.21.14:3000";
-export default BASE_API_IRL = "https://cs24-sdgp-trip-ceylon-production-b031.up.railway.app";
--- Front End/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const supabaseUrl = 'https://dgutsdkqqsbfcfgzxsuc.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRndXRzZGtxcXNiZmNmZ3p4c3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MjI1NTgsImV4cCI6MjA1NTk5ODU1OH0.IXX2IZWJs6o7-Ep4Ii6TB_L3iireXD6hcBwbeSsZktE'
